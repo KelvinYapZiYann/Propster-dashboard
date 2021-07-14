f4c3ed554b1518ddbf9b4bd3ecec34b1d9952387
Yap Zi Yann <32502408+KelvinYapZiYann@users.noreply.github.com>
2021-07-14 12:01:47 +0800

    minor

--- src/pages/Resources/AssetExpenses/AssetExpensesDetail.vue
+++ src/pages/Resources/AssetExpenses/AssetExpensesDetail.vue
@@ -35,10 +35,10 @@ import PaymentPage from "@/components/Resources/PaymentRecords/PaymentPage";
 
 let detailHeaders = {
   payment_description: "Payment Description",
-  amount: "Receiver",
+  amount: "Amount (RM)",
   payment_method: "Payment Method",
   payment_type: "Payment Type",
-  is_reference_only: "Is reference only?"
+  // is_reference_only: "Is reference only?"
 };
 
 export default {
