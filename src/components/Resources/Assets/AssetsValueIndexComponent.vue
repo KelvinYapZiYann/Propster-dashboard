976b9ebf98d0c568ad841730da4100ef895f6e51
Yap Zi Yann <32502408+KelvinYapZiYann@users.noreply.github.com>
2021-08-02 13:47:54 +0800

    dashboard added asset value over time, market rental rate, cashflow, vacancy

---
+++ src/components/Resources/Assets/AssetsValueIndexComponent.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <card>
+        <h4 slot="header" class="card-title text-left">{{$t('dashboard.assetsValueOverTime')}}</h4>
+        <!-- <div class="row">
+          <div class="col-xl-4 col-lg-5 col-md-6 ml-auto">
+            <base-input 
+                    addonLeftIcon="el-icon-search"
+                    :placeholder="$t('component.search')"
+                    v-model="searchQuery">
+            </base-input>
+          </div>
+        </div> -->
+        <div class="table-responsive">
+          <base-table
+            :data="resource.models"
+            :columns="table.columns"
+            thead-classes="text-primary"
+            v-on:show-details="showDetails"
+            :disableEdit="true"
+            :disableDelete="true"
+          >
+          <!-- v-on:show-details="showDetails"
+            v-on:edit-details="editDetails"
+            v-on:delete-details="deleteDetails" -->
+          </base-table>
+          <div
+            slot="footer"
+            class="col-12 d-flex justify-content-center justify-content-sm-between flex-wrap"
+          >
+            <div class="">
+              <p class="card-category">
+                {{$t('component.showing')}} {{ resource.data.from ? resource.data.from : "0" }} {{$t('component.to')}} {{ resource.data.to ? resource.data.to : "0" }} {{$t('component.of')}} {{ resource.data.total }} {{$t('component.entries')}}
+              </p>
+            </div>
+            <base-pagination
+              class="pagination-no-border"
+              v-model="resource.data.currentPage"
+              :per-page="resource.data.perPage"
+              :total="resource.data.total"
+              @input="handlePagination"
+              type="info"
+            >
+            </base-pagination>
+          </div>
+        </div>
+      </card>
+    </div>
+  </div>
+</template>
+<script>
+import {BaseInput, BaseTable, BasePagination, Card} from "@/components";
+import router from "@/router";
+
+export default {
+  components: {
+    BaseInput,
+    BaseTable,
+    BasePagination,
+    Card
+  },
+  data() {
+    return {
+      table: {
+        columns: {
+          asset_nickname: this.$t('property.assetNickname'),
+          asset_purchased_value: this.$t('property.assetPurchasedValue'),
+          asset_current_value: this.$t('property.assetCurrentValue'),
+        },
+      },
+      searchQuery: "",
+      searchQueryTimeout: null,
+    };
+  },
+  props: {
+    resource: {
+      type: Object,
+      required: true,
+      default: {
+        models: [{}],
+        data: {
+          canAdd: false,
+          currentPage: 1,
+          total: 0,
+          from: 0,
+          to: 0,
+          perPage: 10,
+          links: []
+        }
+      },
+      description: "Resource info"
+    },
+    query: {
+      type: Object,
+      // default: {},
+    }
+  },
+  methods: {
+    showDetails(id) {
+      router.push({
+        name: "Asset Detail",
+        params: {
+          assetId: id,
+          previousRoute: this.$router.currentRoute.fullPath
+        }
+      });
+    },
+    // editDetails(id) {
+    //   router.push({
+    //     name: "Edit Assets",
+    //     params: {
+    //       assetId: id,
+    //       previousRoute: this.$router.currentRoute.fullPath
+    //     }
+    //   });
+    // },
+    // deleteDetails(id) {
+    //   if (id == null) {
+    //     this.$notify({
+    //       message:'Server error del id == null',
+    //       icon: 'tim-icons icon-bell-55',
+    //       type: 'danger'
+    //     });
+    //   } else {
+    //     try {
+    //       this.$store.dispatch('asset/remove', id)
+    //       this.$notify({
+    //         message:'Successfully Deleted',
+    //         icon: 'tim-icons icon-bell-55',
+    //         type: 'success'
+    //       });
+    //       this.getResource();
+    //     } catch (e) {
+    //       this.$notify({
+    //         message:'Server error when del',
+    //         icon: 'tim-icons icon-bell-55',
+    //         type: 'danger'
+    //       });
+    //     }
+    //   }
+    // },
+    getResource() {
+      this.$emit('getResource')
+    },
+    async handlePagination(pageId) {
+      try {
+        await this.$store.dispatch('asset/get', pageId).then(() => {
+            this.resource.models = this.$store.getters["asset/models"];
+            this.resource.data = Object.assign({}, this.$store.getters["asset/data"]);
+        });
+      } catch (e) {
+        this.$notify({
+          message:'Server error',
+          icon: 'tim-icons icon-bell-55',
+          type: 'danger'
+        });
+      }
+    }
+  },
+  watch: {
+    searchQuery(value) {
+      if (this.searchQueryTimeout) {
+        clearTimeout(this.searchQueryTimeout);
+      }
+      this.searchQueryTimeout = setTimeout(() => {
+        console.log('searching query with = ' + value);
+      }, 2000);
+    }
+  }
+};
+</script>
+<style>
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,25 +1,6 @@
 <template>
   <div class="content col-xl-10 col-lg-12 col-md-12 ml-auto mr-auto">
     <div class="row">
-      <!-- <div
-        class="col-xl-3 col-lg-6 col-md-6 col-sm-6"
-        v-for="card in statsCards"
-        :key="card.title"
-      >
-        <el-tooltip
-          :content="card.description"
-          :open-delay="300"
-        >
-          <stats-card
-            :title="card.title"
-            :sub-title="card.subTitle"
-            :type="card.type"
-            :icon="card.icon"
-          >
-            <div slot="footer" v-html="card.footer"></div>
-          </stats-card>
-        </el-tooltip>
-      </div> -->
       <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
         <el-tooltip
           :content="$t('dashboard.overdueIncomeDesc')"
@@ -88,8 +69,8 @@
             <div class="col-6">
               <div class="chart-area">
                 <pie-chart
-                  :chart-data="pieChart2.chartData"
-                  :extra-options="pieChart2.extraOptions"
+                  :chart-data="rentChart"
+                  :extra-options="pieChartExtraOptions"
                   :height="120"
                 >
                 </pie-chart>
@@ -105,10 +86,73 @@
         </card>
       </div>
     </div>
+
     <overdue-tenants-index-component
       :resource="resource.overdueTenantList"
       @getResource="getResource"
     ></overdue-tenants-index-component>
+
+    <assets-value-index-component
+      :resource="resource.assetsValueList"
+      @getResource="getResource"
+    ></assets-value-index-component>
+
+    <assets-rental-rate-index-component
+      :resource="resource.rentalRateList"
+      @getResource="getResource"
+    ></assets-rental-rate-index-component>
+
+    <div class="row">
+      <div class="col-md-12 mr-auto">
+        <card class="card-chart card-chart-pie">
+          <h5 slot="header" class="card-category text-left">
+            {{$t('dashboard.cashflow')}}
+          </h5>
+          <div class="row ml-3">
+            <div class="col-12">
+              <p class="category text-left"><i class="tim-icons icon-tag text-info"></i> {{$t('dashboard.income')}}</p>
+              <p class="category text-left"><i class="tim-icons icon-tag text-warning"></i> {{$t('dashboard.expenses')}}</p>
+            </div>
+          </div>
+          <div class="chart-area">
+            <bar-chart
+              :chart-data="cashflowChart"
+              :extra-options="barChartExtraOptions"
+              :height="200"
+            >
+            </bar-chart>
+          </div>
+        </card>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 mr-auto">
+        <card class="card-chart card-chart-pie">
+          <h5 slot="header" class="card-category text-left">
+            {{$t('dashboard.assetsVacancy')}}
+          </h5>
+
+          <div class="row">
+            <div class="col-6">
+              <div class="chart-area">
+                <pie-chart
+                  :chart-data="assetsVacancyChart"
+                  :extra-options="pieChartExtraOptions"
+                  :height="120"
+                >
+                </pie-chart>
+              </div>
+            </div>
+
+            <div class="col-6">
+              <p class="category text-left"><i class="tim-icons icon-tag text-warning"></i> {{$t('dashboard.blankVacancy')}}</p>
+              <p class="category text-left"><i class="tim-icons icon-tag text-info"></i> {{$t('dashboard.currentTenants')}}</p>
+            </div>
+          </div>
+        </card>
+      </div>
+    </div>
     
   </div>
 </template>
@@ -118,21 +162,22 @@ import { Card } from "@/components/index";
 import * as chartConfigs from "@/components/Charts/config";
 import StatsCard from "@/components/Cards/StatsCard";
 import PieChart from "@/components/Charts/PieChart";
+import BarChart from "@/components/Charts/BarChart";
 import OverdueTenantsIndexComponent from "@/components/Resources/Tenants/OverdueTenantsIndexComponent";
+import AssetsValueIndexComponent from "@/components/Resources/Assets/AssetsValueIndexComponent";
+import AssetsRentalRateIndexComponent from "@/components/Resources/Assets/AssetsRentalRateIndexComponent";
 
 export default {
   components: {
     Card,
     StatsCard,
     PieChart,
-    OverdueTenantsIndexComponent
+    BarChart,
+    OverdueTenantsIndexComponent,
+    AssetsValueIndexComponent,
+    AssetsRentalRateIndexComponent
   },
   data() {
-    // let rentalDetail = {
-    //   collected_rent: 10,
-    //   overdue_rent: 10,
-    //   upcoming_rent: 10,
-    // };
     return {
       resource: {
         general: {},
@@ -141,79 +186,82 @@ export default {
           models: [],
           data: {}
         },
-      },
-      // statsCards: [
-      //   {
-      //     title: "RM22,000.00",
-      //     subTitle: this.$t('dashboard.overdueIncome'),
-      //     type: "warning",
-      //     icon: "fas fa-clock",
-      //     description: this.$t('dashboard.overdueIncomeDesc'),
-      //   },
-      //   {
-      //     title: "RM22,000.00",
-      //     subTitle: this.$t('dashboard.upcomingIncome'),
-      //     type: "primary",
-      //     icon: "fas fa-building",
-      //     description: this.$t('dashboard.upcomingIncomeDesc'),
-      //   },
-      //   {
-      //     title: "RM8,000.00",
-      //     subTitle: this.$t('dashboard.overdueExpenses'),
-      //     type: "warning",
-      //     icon: "fas fa-clock",
-      //     description: this.$t('dashboard.overdueExpensesDesc'),
-      //   },
-      //   {
-      //     title: "RM5,000.00",
-      //     subTitle: this.$t('dashboard.upcomingExpenses'),
-      //     type: "primary",
-      //     icon: "fas fa-dollar-sign",
-      //     description: this.$t('dashboard.upcomingExpensesDesc'),
-      //   }
-      // ],
-      pieChart2: {
-        chartData: {
-          labels: [this.$t('dashboard.collectedRent'), this.$t('dashboard.overdueRent'), this.$t('dashboard.upcomingRent')],
-          datasets: [
-            {
-              label: "Rent",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              backgroundColor: ["#ff8779", "#2a84e9", "#e2e2e2"],
-              borderWidth: 0,
-              data: [this.collectedRent, this.overdueRent, this.upcomingRent]
-            }
-          ]
+        assetsValueList: {
+          models: [],
+          data: {}
         },
-        extraOptions: chartConfigs.pieChartOptions
-      }
+        rentalRateList: {
+          models: [],
+          data: {}
+        },
+        assetsVacancy: {
+          currentTenants: 0
+        }
+      },
+      barChartExtraOptions: chartConfigs.barChartOptionsGradient,
+      pieChartExtraOptions: chartConfigs.pieChartOptions,
     };
   },
-  // props: {
-    // fromLogin: {
-    //   type: Boolean,
-    //   required: false,
-    //   default: false,
-    //   description: "From Login"
-    // }
-  // },
   computed: {
-    // enableRTL() {
-    //   return this.$route.query.enableRTL;
-    // },
-    // isRTL() {
-    //   return this.$rtl.isRTL;
-    // }
-    collectedRent() {
-      return this.resource.rental ? this.resource.rental.collected_rent : 0;
+    rentChart() {
+      return {
+        labels: [this.$t('dashboard.collectedRent'), this.$t('dashboard.overdueRent'), this.$t('dashboard.upcomingRent')],
+        datasets: [
+          {
+            label: this.$t('dashboard.rent'),
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            backgroundColor: ["#ff8779", "#2a84e9", "#e2e2e2"],
+            borderWidth: 0,
+            data: [this.resource.rental.collected_rent, this.resource.rental.overdue_rent, this.resource.rental.upcoming_rent]
+          }
+        ]
+      }
     },
-    overdueRent() {
-      return this.resource.rental ? this.resource.rental.overdue_rent : 0;
+    cashflowChart() {
+      return {
+        labels: [this.$t('dateTime.march'), this.$t('dateTime.april'), this.$t('dateTime.may'), this.$t('dateTime.june'), this.$t('dateTime.july'), this.$t('dateTime.august')],
+        datasets: [
+          {
+            label: this.$t('dashboard.income') + " (RM)",
+            fill: true,
+            backgroundColor: "#2a84e9",
+            hoverBackgroundColor: "#2a84e9",
+            borderColor: "#2a84e9",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: [80, 100, 70, 80, 120, 80]
+          },
+          {
+            label: this.$t('dashboard.expenses') + " (RM)",
+            fill: true,
+            backgroundColor: "#ff8779",
+            hoverBackgroundColor: "#ff8779",
+            borderColor: "#ff8779",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: [60, 110, 90, 70, 90, 100]
+          }
+        ]
+      }
     },
-    upcomingRent() {
-      return this.resource.rental ? this.resource.rental.upcoming_rent : 0;
-    }
+    assetsVacancyChart() {
+      return {
+        labels: [this.$t('dashboard.blankVacancy'), this.$t('dashboard.currentTenants')],
+        datasets: [
+          {
+            label: this.$t('dashboard.assetsVacancy'),
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            backgroundColor: ["#ff8779", "#2a84e9"],
+            borderWidth: 0,
+            data: [3, this.resource.assetsVacancy.currentTenants]
+          }
+        ]
+      }
+    },
   },
   methods: {
     async getResource() {
@@ -229,6 +277,20 @@ export default {
         });
         await this.$store.dispatch('dashboard/getOverdueTenantList', {}).then(() => {
           this.resource.overdueTenantList.models = this.$store.getters["dashboard/overdueTenantList"]
+        });
+        await this.$store.dispatch('asset/get', {}).then(() => {
+          this.resource.assetsValueList.models = this.$store.getters["asset/models"]
+          this.resource.assetsValueList.data = Object.assign({}, this.$store.getters["asset/data"]);
+
+          this.resource.rentalRateList.models = this.$store.getters["asset/models"];
+            for (let i = 0; i < this.resource.rentalRateList.models.length; i++) {
+                this.resource.rentalRateList.models[i]['current_rental_rate'] = '1250';
+                this.resource.rentalRateList.models[i]['current_market_rental_rate'] = '1450';
+            }
+            this.resource.rentalRateList.data = Object.assign({}, this.$store.getters["asset/data"]);
+        });
+        await this.$store.dispatch('tenant/get', {}).then(() => {
+          this.resource.assetsVacancy.currentTenants = this.$store.getters["tenant/data"].total;
         });
       } catch (e) {
         this.$notify({
